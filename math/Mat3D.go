6a1646cad4974010f0260d6e0e8b1c4642b6b491
Bhaumik <bhaumik303@gmail.com>
2024-11-12 08:20:00 +0530

    mat 3

--- math/Mat3D.go
+++ math/Mat3D.go
@@ -1,15 +1,8 @@
 package math
 
-import "errors"
+type Mat3D [3][3]float64
 
-var (
-	ErrZeroDet    = errors.New("Determinat is Zero")
-	ErrInvalidLen = errors.New("Invalid Len Slice")
-)
-
-type Mat2D [2][2]float64
-
-func (m *Mat2D) Set(mat [][]float64) error {
+func (m *Mat3D) Set(mat [][]float64) error {
 	if len(mat) < 2 || len(mat[0]) < 2 || len(mat[1]) < 2 {
 		return ErrInvalidLen
 	}
@@ -22,90 +15,90 @@ func (m *Mat2D) Set(mat [][]float64) error {
 	return nil
 }
 
-func (m *Mat2D) SetZero() {
+func (m *Mat3D) SetZero() {
 	m[0][0] = 0
 	m[0][1] = 0
 	m[1][0] = 0
 	m[1][1] = 0
 }
 
-func (m *Mat2D) SetIdentity() {
+func (m *Mat3D) SetIdentity() {
 	m[0][0] = 1
 	m[0][1] = 0
 	m[1][0] = 0
 	m[1][1] = 1
 }
 
-func (m *Mat2D) Add(mat Mat2D) {
+func (m *Mat3D) Add(mat Mat3D) {
 	m[0][0] += mat[0][0]
 	m[0][1] += mat[0][1]
 	m[1][0] += mat[1][0]
 	m[1][1] += mat[1][1]
 }
 
-func (m Mat2D) AddMat(mat Mat2D) Mat2D {
+func (m Mat3D) AddMat(mat Mat3D) Mat3D {
 	m.Add(mat)
 	return m
 }
 
-func (m *Mat2D) Sub(mat Mat2D) {
+func (m *Mat3D) Sub(mat Mat3D) {
 	m[0][0] -= mat[0][0]
 	m[0][1] -= mat[0][1]
 	m[1][0] -= mat[1][0]
 	m[1][1] -= mat[1][1]
 }
 
-func (m Mat2D) SubMat(mat Mat2D) Mat2D {
+func (m Mat3D) SubMat(mat Mat3D) Mat3D {
 	m.Sub(mat)
 	return m
 }
-func (m *Mat2D) Scale(fac float64) {
+func (m *Mat3D) Scale(fac float64) {
 	m[0][0] *= fac
 	m[0][1] *= fac
 	m[1][0] *= fac
 	m[1][1] *= fac
 }
 
-func (m Mat2D) ScaleMat(fac float64) Mat2D {
+func (m Mat3D) ScaleMat(fac float64) Mat3D {
 	m.Scale(fac)
 	return m
 }
 
-func (m *Mat2D) ScaleByVec2D(vec Vec2D) {
+func (m *Mat3D) ScaleByVec2D(vec Vec2D) {
 	m[0][0] *= vec.x
 	m[0][1] *= vec.y
 	m[1][0] *= vec.x
 	m[1][1] *= vec.y
 }
 
-func (m *Mat2D) Transpose() {
+func (m *Mat3D) Transpose() {
 	m[0][1], m[1][0] = m[1][0], m[0][1]
 }
 
-func (m Mat2D) TranposeMat() Mat2D {
+func (m Mat3D) TranposeMat() Mat3D {
 	m.Transpose()
 	return m
 }
 
-func (m Mat2D) Det() float64 {
+func (m Mat3D) Det() float64 {
 	return (m[0][0] * m[1][1]) - (m[0][1] * m[1][0])
 }
 
-func (m *Mat2D) ToAdjoint() {
+func (m *Mat3D) ToAdjoint() {
 	m[0][0], m[1][1] = m[1][1], m[0][0]
 
 	m[0][1] *= -1
 	m[1][0] *= -1
 }
 
-func (m *Mat2D) AdjointMat() Mat2D {
-	return Mat2D{
+func (m *Mat3D) AdjointMat() Mat3D {
+	return Mat3D{
 		{m[1][1], -m[0][1]},
 		{-m[1][0], m[0][0]},
 	}
 }
 
-func (m *Mat2D) Inverse() error {
+func (m *Mat3D) Inverse() error {
 	det := m.Det()
 	if det == 0 {
 		return ErrZeroDet
@@ -117,13 +110,13 @@ func (m *Mat2D) Inverse() error {
 	return nil
 }
 
-func (m Mat2D) InverseMat() Mat2D {
+func (m Mat3D) InverseMat() Mat3D {
 	m.Inverse()
 	return m
 }
 
-func (m Mat2D) Multiply(mat Mat2D) Mat2D {
-	out := Mat2D{}
+func (m Mat3D) Multiply(mat Mat3D) Mat3D {
+	out := Mat3D{}
 
 	for k := 0; k < 2; k++ {
 		for i := 0; i < 2; i++ {
@@ -136,11 +129,11 @@ func (m Mat2D) Multiply(mat Mat2D) Mat2D {
 	return out
 }
 
-func (m *Mat2D) IsEqual(mat Mat2D) bool {
+func (m *Mat3D) IsEqual(mat Mat3D) bool {
 	return m[0][0] == mat[0][0] && m[0][1] == mat[0][1] &&
 		m[1][0] == mat[1][0] && m[1][1] == mat[1][1]
 }
 
-func (m *Mat2D) Trace() float64 {
+func (m *Mat3D) Trace() float64 {
 	return m[0][0] + m[1][1]
 }
