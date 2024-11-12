d0411c73b11cd9179a5fbc289c961afeb3eabd9b
Bhaumik <bhaumik303@gmail.com>
2024-11-12 19:12:03 +0530

    mat 3

--- math/Mat3D.go
+++ math/Mat3D.go
@@ -3,14 +3,21 @@ package math
 type Mat3D [3][3]float64
 
 func (m *Mat3D) Set(mat [][]float64) error {
-	if len(mat) < 2 || len(mat[0]) < 2 || len(mat[1]) < 2 {
+	if len(mat) < 3 || len(mat[0]) < 3 || len(mat[1]) < 3 || len(mat[2]) < 3 {
 		return ErrInvalidLen
 	}
 
 	m[0][0] = mat[0][0]
 	m[0][1] = mat[0][1]
+	m[0][2] = mat[0][2]
+
 	m[1][0] = mat[1][0]
 	m[1][1] = mat[1][1]
+	m[1][2] = mat[1][2]
+
+	m[2][0] = mat[2][0]
+	m[2][1] = mat[2][1]
+	m[2][2] = mat[2][2]
 
 	return nil
 }
@@ -18,22 +25,43 @@ func (m *Mat3D) Set(mat [][]float64) error {
 func (m *Mat3D) SetZero() {
 	m[0][0] = 0
 	m[0][1] = 0
+	m[0][2] = 0
+
 	m[1][0] = 0
 	m[1][1] = 0
+	m[1][2] = 0
+
+	m[2][0] = 0
+	m[2][1] = 0
+	m[2][2] = 0
 }
 
 func (m *Mat3D) SetIdentity() {
 	m[0][0] = 1
 	m[0][1] = 0
+	m[0][2] = 0
+
 	m[1][0] = 0
 	m[1][1] = 1
+	m[1][2] = 0
+
+	m[2][0] = 0
+	m[2][1] = 0
+	m[2][2] = 1
 }
 
 func (m *Mat3D) Add(mat Mat3D) {
 	m[0][0] += mat[0][0]
 	m[0][1] += mat[0][1]
+	m[0][2] += mat[0][2]
+
 	m[1][0] += mat[1][0]
 	m[1][1] += mat[1][1]
+	m[1][2] += mat[1][2]
+
+	m[2][0] += mat[2][0]
+	m[2][1] += mat[2][1]
+	m[2][2] += mat[2][2]
 }
 
 func (m Mat3D) AddMat(mat Mat3D) Mat3D {
@@ -44,8 +72,15 @@ func (m Mat3D) AddMat(mat Mat3D) Mat3D {
 func (m *Mat3D) Sub(mat Mat3D) {
 	m[0][0] -= mat[0][0]
 	m[0][1] -= mat[0][1]
+	m[0][2] -= mat[0][2]
+
 	m[1][0] -= mat[1][0]
 	m[1][1] -= mat[1][1]
+	m[1][2] -= mat[1][2]
+
+	m[2][0] -= mat[2][0]
+	m[2][1] -= mat[2][1]
+	m[2][2] -= mat[2][2]
 }
 
 func (m Mat3D) SubMat(mat Mat3D) Mat3D {
@@ -55,8 +90,15 @@ func (m Mat3D) SubMat(mat Mat3D) Mat3D {
 func (m *Mat3D) Scale(fac float64) {
 	m[0][0] *= fac
 	m[0][1] *= fac
+	m[0][2] *= fac
+
 	m[1][0] *= fac
 	m[1][1] *= fac
+	m[1][2] *= fac
+
+	m[2][0] *= fac
+	m[2][1] *= fac
+	m[2][2] *= fac
 }
 
 func (m Mat3D) ScaleMat(fac float64) Mat3D {
@@ -64,15 +106,24 @@ func (m Mat3D) ScaleMat(fac float64) Mat3D {
 	return m
 }
 
-func (m *Mat3D) ScaleByVec2D(vec Vec2D) {
+func (m *Mat3D) ScaleByVec2D(vec Vec3D) {
 	m[0][0] *= vec.x
 	m[0][1] *= vec.y
+	m[0][2] *= vec.z
+
 	m[1][0] *= vec.x
 	m[1][1] *= vec.y
+	m[1][2] *= vec.z
+
+	m[2][0] *= vec.x
+	m[2][1] *= vec.y
+	m[2][2] *= vec.z
 }
 
 func (m *Mat3D) Transpose() {
 	m[0][1], m[1][0] = m[1][0], m[0][1]
+	m[0][2], m[2][0] = m[2][0], m[0][2]
+	m[2][1], m[1][2] = m[1][2], m[2][1]
 }
 
 func (m Mat3D) TranposeMat() Mat3D {
